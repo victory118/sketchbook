1b57ebbe3d823c5c35e8bb6d7e4ff30b605b1bc9
Victor Yu <victory118@gmail.com>
2018-08-22 18:16:10 -0400

    added working ros publisher to command motor power in readEncoder_ros.ino

--- readEncoder_ros/readEncoder_ros.ino
+++ readEncoder_ros/readEncoder_ros.ino
@@ -6,14 +6,6 @@
 #include <std_msgs/Float32.h>
 #include <TimerOne.h>
 
-ros::NodeHandle nh;
-
-std_msgs::Float32 left_wheel_vel;
-ros::Publisher left_wheel_vel_pub("/left_wheel_velocity", &left_wheel_vel);
-
-std_msgs::Float32 right_wheel_vel;
-ros::Publisher right_wheel_vel_pub("/right_wheel_velocity", &right_wheel_vel);
-
 // left encoder variables
 const int clkPinL = 2;
 const int dirPinL = 8;
@@ -33,6 +25,45 @@ const unsigned long sampleTime_us = 200000; // 200000us = 0.2s = 5Hz
 const float sampleRate_hz = 1.0/(sampleTime_us/1e6);
 const float pi = 3.1415;
 const int ppr = 1920; // encoder pulses per revolution of motor
+
+// motor variables
+const int motPwmPinL = 6;
+const int motDirPinL = 7;
+const int motPwmPinR = 5;
+const int motDirPinR = 4;
+
+ros::NodeHandle nh;
+
+std_msgs::Float32 left_wheel_vel;
+ros::Publisher left_wheel_vel_pub("/left_wheel_velocity", &left_wheel_vel);
+
+std_msgs::Float32 right_wheel_vel;
+ros::Publisher right_wheel_vel_pub("/right_wheel_velocity", &right_wheel_vel);
+
+void turnWheel( const std_msgs::Float32 &wheel_power,
+                const int pwmPin,
+                const int dirPin ) {
+    float factor = max(min(wheel_power.data, 1.0f), -1.0f);
+    if( factor >= 0 ) {
+        digitalWrite(dirPin, LOW);
+        analogWrite(pwmPin, (unsigned int)(255 * factor));
+    } else {
+        digitalWrite(dirPin, HIGH);
+        analogWrite(pwmPin, (unsigned int)(255 * (1.0f + factor)));
+    }   
+}
+
+void rightWheelCb( const std_msgs::Float32 &wheel_power ) {
+    turnWheel( wheel_power, motPwmPinR, motDirPinR );
+}
+void leftWheelCb( const std_msgs::Float32 &wheel_power ) {
+    turnWheel( wheel_power, motPwmPinL, motDirPinL );
+}
+
+ros::Subscriber<std_msgs::Float32> sub_right("wheel_power_right",
+                                            &rightWheelCb );
+ros::Subscriber<std_msgs::Float32> sub_left("wheel_power_left",
+                                           &leftWheelCb );
 
 void timerIsr(void) {
   Timer1.detachInterrupt(); // stop the timer
@@ -77,6 +108,8 @@ void encoderIntR() {
 
 void setup() {
 //  Serial.begin(9600);
+
+  // encoders
   pinMode(clkPinL, INPUT);  
   pinMode(dirPinL, INPUT);
   attachInterrupt(0, encoderIntL, RISING);
@@ -85,12 +118,20 @@ void setup() {
   pinMode(dirPinR, INPUT);
   attachInterrupt(1, encoderIntR, RISING);
   
+  // motors
+  pinMode(motPwmPinL, OUTPUT);
+  pinMode(motDirPinL, OUTPUT);
+  pinMode(motPwmPinR, OUTPUT);
+  pinMode(motDirPinR, OUTPUT);
+  
   Timer1.initialize(sampleTime_us); //initialize timer1 with period sampleTime_us
   Timer1.attachInterrupt(timerIsr); //attaches timerIsr function every period
   
   nh.initNode();
   nh.advertise(left_wheel_vel_pub);
   nh.advertise(right_wheel_vel_pub);
+  nh.subscribe(sub_right);
+  nh.subscribe(sub_left);
   
 }
 
