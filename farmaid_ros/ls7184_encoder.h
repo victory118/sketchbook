89a0ff1fbee2bc070c17fdd0321aa234c77c85a6
Victor Yu <victory118@gmail.com>
2019-04-16 22:50:47 -0400

    moved encoder interrupt callback functions to their own files

--- farmaid_ros/ls7184_encoder.h
+++ farmaid_ros/ls7184_encoder.h
@@ -82,33 +82,4 @@ namespace Farmaid
     };
 };
 
-extern Farmaid::EncoderParams left_encoder_p;
-extern Farmaid::EncoderParams right_encoder_p;
-
-// Encoder counter variables are global because they use hardware interrupts
-volatile int left_encoder_count = 0;
-volatile boolean left_encoder_change_flag = false;
-volatile int right_encoder_count = 0;
-volatile boolean right_encoder_change_flag = false;
-
-void LeftEncoderInterrupt() {
-  if (digitalRead(left_encoder_p.dir_pin) == HIGH) {
-    left_encoder_count++;
-  }
-  else {
-    left_encoder_count--;
-  }
-  left_encoder_change_flag = true;
-}
-
-void RightEncoderInterrupt() {
-  if (digitalRead(right_encoder_p.dir_pin) == LOW) {
-    right_encoder_count++;
-  }
-  else {
-    right_encoder_count--;
-  }
-  right_encoder_change_flag = true;
-}
-
 #endif
