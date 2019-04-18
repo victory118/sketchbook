2dd4e215956dc9978b528108954bc9052873b11b
Victor Yu <victory118@gmail.com>
2019-04-17 20:23:38 -0400

    Moved initialization of parameters, ROS functions, and encoder interrupt callback functions to their own .h files. Also wrote tests for Mega 4wd robot configuration.

--- farmaid_ros/farmaid_ros.ino
+++ farmaid_ros/farmaid_ros.ino
@@ -6,13 +6,11 @@
 #define TESTING // initialize motor, encoder, and pid controller objects individually for testing
 //#define ROS_TELEOP // initialize robot object only when connected to ROS for teleop
 
-#include <Arduino.h>
-#include "pid_controller.h"
+//#include <Arduino.h>
 
 #ifdef ROS_TELEOP
 
-#include <ros.h>
-#include <geometry_msgs/Twist.h>
+#include "ros_setup.h"
 
 #endif
 
@@ -24,30 +22,7 @@ const unsigned long serial_period = 100; // period for printing to terminal for 
 
 #ifdef UNO_2WD
 
-#include "drv8833_motor.h"
-#include "ls7184_encoder.h"
-#include "robot.h"
-
-const float wheelbase = 0.14; // [m]
-const float wheel_radius = 0.065 / 2; // [m]
-const float no_load_rps = 0.4 / wheel_radius; // determined experimentally in [m/s] --> [rad/s]
-
-// Initialize encoder parameter struct
-// {byte clk_pin (byte), byte dir_pin, float counts_per_rev, float sample_period, float filt_tc}
-Farmaid::EncoderParams left_encoder_p = {2, 8, 1920.0, control_period / 1000.0, 4.0 * control_period / 1000.0};
-Farmaid::EncoderParams right_encoder_p = {3, 9, 1920.0, control_period / 1000.0, 4.0 * control_period / 1000.0};
-
-// Initialize motor parameter struct
-// {byte pwm_pin, byte dir_pin, unsigned int max_command, float no_load_rps}
-Farmaid::MotorParams left_motor_p = {6, 7, 255, no_load_rps};
-Farmaid::MotorParams right_motor_p = {5, 4, 255, no_load_rps};
-
-// Initialize PID controller parameter struct
-// {float p_gain, float i_gain, float d_gain, float filt_const, float sample_period}
-Farmaid::PidParams left_pid_p = {1.0, 0.0, 0.0, 0.0, control_period / 1000.0};
-Farmaid::PidParams right_pid_p = {1.0, 0.0, 0.0, 0.0, control_period / 1000.0};
-
-#include "uno_2wd_helper.h" // encoder interrupt callback functions
+#include "uno_2wd_setup.h" // initialize motor, encoder, and PID parameters and encoder interrupt callback functions
 
 #ifdef TESTING
 
@@ -84,36 +59,7 @@ Farmaid::Robot robot = Farmaid::Robot(Farmaid::Encoder(left_encoder_p, &left_enc
 
 #ifdef MEGA_4WD
 
-#include "drv8833_motor.h"
-#include "ls7184_encoder.h"
-#include "robot.h"
-
-const float wheelbase = 0.14; // [m]
-const float wheel_radius = 0.065 / 2; // [m]
-const float no_load_rps = 0.4 / wheel_radius; // determined experimentally in [m/s] --> [rad/s]
-
-// Initialize encoder parameter struct
-// {byte clk_pin (byte), byte dir_pin, float counts_per_rev, float sample_period, float filt_tc}
-Farmaid::EncoderParams fleft_encoder_p = {2, 22, 1920.0, control_period / 1000.0, 4.0 * control_period / 1000.0};
-Farmaid::EncoderParams fright_encoder_p = {3, 23, 1920.0, control_period / 1000.0, 4.0 * control_period / 1000.0};
-Farmaid::EncoderParams rright_encoder_p = {18, 24, 1920.0, control_period / 1000.0, 4.0 * control_period / 1000.0};
-Farmaid::EncoderParams rleft_encoder_p = {19, 25, 1920.0, control_period / 1000.0, 4.0 * control_period / 1000.0};
-
-// Initialize motor parameter struct
-// {byte pwm_pin, byte dir_pin, unsigned int max_command, float no_load_rps}
-Farmaid::MotorParams fleft_motor_p = {5, 4, 255, no_load_rps};
-Farmaid::MotorParams fright_motor_p = {7, 6, 255, no_load_rps};
-Farmaid::MotorParams rright_motor_p = {9, 8, 255, no_load_rps};
-Farmaid::MotorParams rleft_motor_p = {11, 10, 255, no_load_rps};
-
-// Initialize PID controller parameter struct
-// {float p_gain, float i_gain, float d_gain, float filt_const, float sample_period}
-Farmaid::PidParams fleft_pid_p = {1.0, 0.0, 0.0, 0.0, control_period / 1000.0};
-Farmaid::PidParams fright_pid_p = {1.0, 0.0, 0.0, 0.0, control_period / 1000.0};
-Farmaid::PidParams rright_pid_p = {1.0, 0.0, 0.0, 0.0, control_period / 1000.0};
-Farmaid::PidParams rleft_pid_p = {1.0, 0.0, 0.0, 0.0, control_period / 1000.0};
-
-#include "mega_4wd_helper.h" // encoder interrupt callback functions  
+#include "mega_4wd_setup.h" // initialize motor, encoder, and PID parameters and encoder interrupt callback functions
 
 #ifdef TESTING
 
@@ -154,45 +100,6 @@ Farmaid::Robot robot = Farmaid::Robot(Farmaid::Encoder(fleft_encoder_p, &fleft_e
 #endif
 #endif
 
-#ifdef ROS_TELEOP
-
-// These includes have to be done in the beginning of the file for some reason?
-//#include <ros.h>
-//#include <geometry_msgs/Twist.h>
-
-// Use this robot initialization when running teleop mode with ROS
-//Farmaid::Robot robot = Farmaid::Robot(Farmaid::Encoder(left_encoder_p, &left_encoder_count), Farmaid::Encoder(right_encoder_p, &right_encoder_count),
-//                                      Farmaid::Motor(left_motor_p), Farmaid::Motor(right_motor_p),
-//                                      Farmaid::PidController(left_pid_p), Farmaid::PidController(right_pid_p),
-//                                      wheelbase, wheel_radius);
-
-unsigned long prev_ros_millis;
-const unsigned long ros_period = 20; // ROS communication period [millis]
-
-// Initialize ROS node handle, publishers, and subscribers
-ros::NodeHandle nh;
-geometry_msgs::Twist cmd_vel;
-
-void RosCallBack(const geometry_msgs::Twist &twist_msg)
-{
-    cmd_vel.linear.x = twist_msg.linear.x;
-    cmd_vel.angular.z = twist_msg.angular.z;
-}
-
-ros::Subscriber <geometry_msgs::Twist> sub("/cmd_vel", RosCallBack);
-
-void UpdateRos()
-{
-    // Publish and subscribe to ROS topics
-    if (curr_millis - prev_ros_millis >= ros_period)
-    {
-        nh.spinOnce();
-        prev_ros_millis = curr_millis;
-    }
-}
-
-#endif
-
 void setup() {
 
     curr_millis = millis();
@@ -218,10 +125,10 @@ void setup() {
 #endif
 
 #ifdef MEGA_4WD
-    attachInterrupt(0, FLeftEncoderInterrupt, RISING);
-    attachInterrupt(1, FRightEncoderInterrupt, RISING);
-    attachInterrupt(5, RRightEncoderInterrupt, RISING);
-    attachInterrupt(4, RLeftEncoderInterrupt, RISING);
+    attachInterrupt(4, FLeftEncoderInterrupt, RISING);
+    attachInterrupt(5, FRightEncoderInterrupt, RISING);
+    attachInterrupt(0, RRightEncoderInterrupt, RISING);
+    attachInterrupt(1, RLeftEncoderInterrupt, RISING);
 #endif
 
     // Necessary for encoder interrupts to initialize
@@ -238,12 +145,25 @@ void loop() {
 #endif
 
     // Test individual components
-    ReadEncoders(); // Passed!
+//    ReadEncoders(); // Passed!
 //    Farmaid::TestEncoderClass(left_encoder, right_encoder); // Passed!
+//    Farmaid::TestEncoderClass(fleft_encoder, fright_encoder); // Passed!
+//    Farmaid::TestEncoderClass(rleft_encoder, rright_encoder); // Passed!
+
+    // Test motor open loop
 //    Farmaid::TestMotorOpenLoop(left_motor, 1); // Passed!
 //    Farmaid::TestMotorOpenLoop(left_motor, -1); // Passed!
 //    Farmaid::TestMotorOpenLoop(right_motor, 1); // Passed!
 //    Farmaid::TestMotorOpenLoop(right_motor, -1); // Passed!
+
+//    Farmaid::TestMotorOpenLoop(fleft_motor, 1); // Passed with Mega 2wd robot
+//    Farmaid::TestMotorOpenLoop(fleft_motor, -1); // Passed with Mega 2wd robot
+//    Farmaid::TestMotorOpenLoop(fright_motor, 1); // Passed with Mega 2wd robot
+//    Farmaid::TestMotorOpenLoop(fright_motor, -1); // Passed with Mega 2wd robot
+//    Farmaid::TestMotorOpenLoop(rleft_motor, 1); // Passed with Mega 2wd robot
+//    Farmaid::TestMotorOpenLoop(rleft_motor, -1); // Passed with Mega 2wd robot
+//    Farmaid::TestMotorOpenLoop(rright_motor, 1); // Passed with Mega 2wd robot
+//    Farmaid::TestMotorOpenLoop(rright_motor, -1); // Passed with Mega 2wd robot
 
     // Find deadband
 //    left_motor.set_command(0.28);
@@ -265,15 +185,51 @@ void loop() {
 //    Farmaid::TestMotorPositionControl(right_motor, right_encoder, right_pid, 1); // Passed!
 //    Farmaid::TestMotorPositionControl(right_motor, right_encoder, right_pid, -1); // Passed!
 
+//    fleft_pid.set_gains(7.5, 0.0, 0.25, 10.0 * control_period / 1000.0);
+//    fleft_pid.set_max_deadband(0.15);
+//    fleft_pid.set_min_deadband(-0.1);
+//    Farmaid::TestMotorPositionControl(fleft_motor, fleft_encoder, fleft_pid, 1); // Passed with Mega 2wd robot
+//    Farmaid::TestMotorPositionControl(fleft_motor, fleft_encoder, fleft_pid, -1); // Passed with Mega 2wd robot
+
+//    fright_pid.set_gains(7.0, 0.0, 0.25, 10.0 * control_period / 1000.0);
+//    fright_pid.set_max_deadband(0.18);
+//    fright_pid.set_min_deadband(-0.1);
+//    Farmaid::TestMotorPositionControl(fright_motor, fright_encoder, fright_pid, 1); // Passed with Mega 2wd robot
+//    Farmaid::TestMotorPositionControl(fright_motor, fright_encoder, fright_pid, -1); // Passed with Mega 2wd robot
+
+//    rleft_pid.set_gains(7.5, 0.0, 0.25, 10.0 * control_period / 1000.0);
+//    rleft_pid.set_max_deadband(0.15);
+//    rleft_pid.set_min_deadband(-0.1);
+//    Farmaid::TestMotorPositionControl(rleft_motor, rleft_encoder, rleft_pid, 1); // Passed with Mega 2wd robot
+//    Farmaid::TestMotorPositionControl(rleft_motor, rleft_encoder, rleft_pid, -1); // Passed with Mega 2wd robot
+
+//    rright_pid.set_gains(7.0, 0.0, 0.25, 10.0 * control_period / 1000.0);
+//    rright_pid.set_max_deadband(0.18);
+//    rright_pid.set_min_deadband(-0.1);
+//    Farmaid::TestMotorPositionControl(rright_motor, rright_encoder, rright_pid, 1); // Passed with Mega 2wd robot
+//    Farmaid::TestMotorPositionControl(rright_motor, rright_encoder, rright_pid, -1); // Passed with Mega 2wd robot
+
     // Find maximum speed of each motor to add feedforward to PID controller
+    
 //    Farmaid::TestMaxSpeed(left_motor, left_encoder, 1); // 13.09 rad/s * 0.065/2 m = 0.425 m/s
 //    Farmaid::TestMaxSpeed(left_motor, left_encoder, -1); // 12.44 rad/s * 0.065/2 m = 0.404 m/s
 //    Farmaid::TestMaxSpeed(right_motor, right_encoder, 1); // 13.09 rad/s * 0.065/2 m = 0.425 m/s
 //    Farmaid::TestMaxSpeed(right_motor, right_encoder, -1); // 13.09 rad/s * 0.065/2 m = 0.425 m/s
 
+//    Farmaid::TestMaxSpeed(fleft_motor, fleft_encoder, 1); // Passed with Mega 2wd robot
+//    Farmaid::TestMaxSpeed(fleft_motor, fleft_encoder, -1); // Passed with Mega 2wd robot
+//    Farmaid::TestMaxSpeed(fright_motor, fright_encoder, 1); // Passed with Mega 2wd robot
+//    Farmaid::TestMaxSpeed(fright_motor, fright_encoder, -1); // Passed with Mega 2wd robot
+
+//    Farmaid::TestMaxSpeed(rleft_motor, rleft_encoder, 1); // Passed with Mega 2wd robot
+//    Farmaid::TestMaxSpeed(rleft_motor, rleft_encoder, -1); // Passed with Mega 2wd robot
+//    Farmaid::TestMaxSpeed(rright_motor, rright_encoder, 1); // Passed with Mega 2wd robot
+//    Farmaid::TestMaxSpeed(rright_motor, rright_encoder, -1); // Passed with Mega 2wd robot
+
     // Motor velocity control
 
     // Step velocity commands with PID only
+    
 //    left_pid.set_gains(12.0, 3.0, 0.1, 10.0 * control_period / 1000.0);
 //    left_pid.set_antiwindup(1.2); // Antiwindup makes it worse. Need to debug this.
 //    Farmaid::TestMotorVelocityStep(left_motor, left_encoder, left_pid, 1); // Passed!
@@ -284,6 +240,28 @@ void loop() {
 //    Farmaid::TestMotorVelocityStep(right_motor, right_encoder, right_pid, 1); // Passed!
 //    right_pid.set_gains(11.0, 2.0, 0.0, 10.0 * control_period / 1000.0);
 //    Farmaid::TestMotorVelocityStep(right_motor, right_encoder, right_pid, -1); // Passed!
+
+//    fleft_pid.set_gains(12.0, 3.0, 0.1, 10.0 * control_period / 1000.0);
+//    fleft_pid.set_antiwindup(1.2); // Antiwindup makes it worse. Need to debug this.
+//    Farmaid::TestMotorVelocityStep(fleft_motor, fleft_encoder, fleft_pid, 1); // Passed with Mega 2wd robot
+//    fleft_pid.set_gains(11.0, 2.5, 0.0, 10.0 * control_period / 1000.0);
+//    Farmaid::TestMotorVelocityStep(fleft_motor, fleft_encoder, fleft_pid, -1); // Passed with Mega 2wd robot
+
+//    fright_pid.set_gains(12.0, 3.0, 0.1, 10.0 * control_period / 1000.0);
+//    Farmaid::TestMotorVelocityStep(fright_motor, fright_encoder, fright_pid, 1); // Passed with Mega 2wd robot
+//    fright_pid.set_gains(11.0, 2.0, 0.0, 10.0 * control_period / 1000.0);
+//    Farmaid::TestMotorVelocityStep(fright_motor, fright_encoder, fright_pid, -1); // Passed with Mega 2wd robot
+
+//    rleft_pid.set_gains(12.0, 3.0, 0.1, 10.0 * control_period / 1000.0);
+//    rleft_pid.set_antiwindup(1.2); // Antiwindup makes it worse. Need to debug this.
+//    Farmaid::TestMotorVelocityStep(rleft_motor, rleft_encoder, rleft_pid, 1); // Passed with Mega 2wd robot
+//    rleft_pid.set_gains(11.0, 2.5, 0.0, 10.0 * control_period / 1000.0);
+//    Farmaid::TestMotorVelocityStep(rleft_motor, rleft_encoder, rleft_pid, -1); // Passed with Mega 2wd robot
+
+//    rright_pid.set_gains(12.0, 3.0, 0.1, 10.0 * control_period / 1000.0);
+//    Farmaid::TestMotorVelocityStep(rright_motor, rright_encoder, rright_pid, 1); // Passed with Mega 2wd robot
+//    rright_pid.set_gains(11.0, 2.0, 0.0, 10.0 * control_period / 1000.0);
+//    Farmaid::TestMotorVelocityStep(rright_motor, rright_encoder, rright_pid, -1); // Passed with Mega 2wd robot
 
     // Step velocity commands with PID and feedforward
 //    left_pid.set_gains(2.0, 0.0, 0.0, 10.0 * control_period / 1000.0);
@@ -296,7 +274,28 @@ void loop() {
 //    Farmaid::TestMotorVelocityStep(right_motor, right_encoder, right_pid, 1); // Passed!
 //    Farmaid::TestMotorVelocityStep(right_motor, right_encoder, right_pid, -1); // Passed!
 
+//    fleft_pid.set_gains(2.0, 0.0, 0.0, 10.0 * control_period / 1000.0);
+//    fleft_pid.set_feedforward_flag(true);
+//    Farmaid::TestMotorVelocityStep(fleft_motor, fleft_encoder, fleft_pid, 1); // Passed with Mega 2wd robot
+//    Farmaid::TestMotorVelocityStep(fleft_motor, fleft_encoder, fleft_pid, -1); // Passed with Mega 2wd robot
+
+//    fright_pid.set_gains(2.0, 0.0, 0.0, 10.0 * control_period / 1000.0);
+//    fright_pid.set_feedforward_flag(true);
+//    Farmaid::TestMotorVelocityStep(fright_motor, fright_encoder, fright_pid, 1); // Passed with Mega 2wd robot
+//    Farmaid::TestMotorVelocityStep(fright_motor, fright_encoder, fright_pid, -1); // Passed with Mega 2wd robot
+
+//    rleft_pid.set_gains(2.0, 0.0, 0.0, 10.0 * control_period / 1000.0);
+//    rleft_pid.set_feedforward_flag(true);
+//    Farmaid::TestMotorVelocityStep(rleft_motor, rleft_encoder, rleft_pid, 1); // Passed with Mega 2wd robot
+//    Farmaid::TestMotorVelocityStep(rleft_motor, rleft_encoder, rleft_pid, -1); // Passed with Mega 2wd robot
+
+//    rright_pid.set_gains(2.0, 0.0, 0.0, 10.0 * control_period / 1000.0);
+//    rright_pid.set_feedforward_flag(true);
+//    Farmaid::TestMotorVelocityStep(rright_motor, rright_encoder, rright_pid, 1); // Passed with Mega 2wd robot
+//    Farmaid::TestMotorVelocityStep(rright_motor, rright_encoder, rright_pid, -1); // Passed with Mega 2wd robot
+
     // Sinusoidal velocity command with PID and feedforward
+    
 //    left_pid.set_gains(3.0, 0.0, 0.0, 10.0 * control_period / 1000.0);
 //    left_pid.set_feedforward_flag(true);
 //    Farmaid::TestMotorVelocitySine(left_motor, left_encoder, left_pid, 1); // Passed!
@@ -306,6 +305,26 @@ void loop() {
 //    right_pid.set_feedforward_flag(true);
 //    Farmaid::TestMotorVelocitySine(right_motor, right_encoder, right_pid, 1); // Passed!
 //    Farmaid::TestMotorVelocitySine(right_motor, right_encoder, right_pid, -1); // Passed!
+
+//    fleft_pid.set_gains(3.0, 0.0, 0.0, 10.0 * control_period / 1000.0);
+//    fleft_pid.set_feedforward_flag(true);
+//    Farmaid::TestMotorVelocitySine(fleft_motor, fleft_encoder, fleft_pid, 1); // Passed with Mega 2wd robot
+//    Farmaid::TestMotorVelocitySine(fleft_motor, fleft_encoder, fleft_pid, -1); // Passed with Mega 2wd robot
+
+//    fright_pid.set_gains(3.0, 0.0, 0.0, 10.0 * control_period / 1000.0);
+//    fright_pid.set_feedforward_flag(true);
+//    Farmaid::TestMotorVelocitySine(fright_motor, fright_encoder, fright_pid, 1); // Passed with Mega 2wd robot
+//    Farmaid::TestMotorVelocitySine(fright_motor, fright_encoder, fright_pid, -1); // Passed with Mega 2wd robot
+
+//    rleft_pid.set_gains(3.0, 0.0, 0.0, 10.0 * control_period / 1000.0);
+//    rleft_pid.set_feedforward_flag(true);
+//    Farmaid::TestMotorVelocitySine(rleft_motor, rleft_encoder, rleft_pid, 1); //
+//    Farmaid::TestMotorVelocitySine(rleft_motor, rleft_encoder, rleft_pid, -1); //
+
+//    rright_pid.set_gains(3.0, 0.0, 0.0, 10.0 * control_period / 1000.0);
+//    rright_pid.set_feedforward_flag(true);
+//    Farmaid::TestMotorVelocitySine(rright_motor, rright_encoder, rright_pid, 1); //
+//    Farmaid::TestMotorVelocitySine(rright_motor, rright_encoder, rright_pid, -1); //
 
     // Test robot drive method (PID gains and feedforward_flag set in Robot constructor)
 //    Farmaid::TestRobotForward(robot); // Passed!
