7613c70aaaf191aa743373babafa64fc1f0eefd9
Victor Yu <victory118@gmail.com>
2019-04-17 20:25:07 -0400

    renamed uno_2wd_helper.h file

--- farmaid_ros/uno_2wd_setup.h
+++ farmaid_ros/uno_2wd_setup.h
@@ -1,12 +1,29 @@
-#ifndef UNO_2WD_HELPER_H
-#define UNO_2WD_HELPER_H
+#ifndef UNO_2WD_SETUP_H
+#define UNO_2WD_SETUP_H
 
-extern unsigned long curr_millis;
-extern unsigned long prev_serial_millis;
-extern const unsigned long serial_period;
+#include "drv8833_motor.h"
+#include "ls7184_encoder.h"
+#include "pid_controller.h"
+#include "robot.h"
 
-extern Farmaid::EncoderParams left_encoder_p;
-extern Farmaid::EncoderParams right_encoder_p;
+const float wheelbase = 0.14; // [m]
+const float wheel_radius = 0.065 / 2; // [m]
+const float no_load_rps = 0.4 / wheel_radius; // determined experimentally in [m/s] --> [rad/s]
+
+// Initialize encoder parameter struct
+// {byte clk_pin (byte), byte dir_pin, float counts_per_rev, float sample_period, float filt_tc}
+Farmaid::EncoderParams left_encoder_p = {2, 8, 1920.0, control_period / 1000.0, 4.0 * control_period / 1000.0};
+Farmaid::EncoderParams right_encoder_p = {3, 9, 1920.0, control_period / 1000.0, 4.0 * control_period / 1000.0};
+
+// Initialize motor parameter struct
+// {byte pwm_pin, byte dir_pin, unsigned int max_command, float no_load_rps}
+Farmaid::MotorParams left_motor_p = {6, 7, 255, no_load_rps};
+Farmaid::MotorParams right_motor_p = {5, 4, 255, no_load_rps};
+
+// Initialize PID controller parameter struct
+// {float p_gain, float i_gain, float d_gain, float filt_const, float sample_period}
+Farmaid::PidParams left_pid_p = {1.0, 0.0, 0.0, 0.0, control_period / 1000.0};
+Farmaid::PidParams right_pid_p = {1.0, 0.0, 0.0, 0.0, control_period / 1000.0};
 
 // Encoder counter variables are global because they use hardware interrupts
 volatile int left_encoder_count = 0;
