5c506036326993ee0fee760001aa6d8e00902c51
Victor Yu <victory118@gmail.com>
2019-04-22 17:13:01 -0400

    added #ifdef to select different motor driver configuration

--- farmaid_ros/mega_4wd_setup.h
+++ farmaid_ros/mega_4wd_setup.h
@@ -1,28 +1,44 @@
 #ifndef MEGA_4WD_HELPER_H
 #define MEGA_4WD_HELPER_H
 
+// Select the motor driver (comment the other ones out)
+//#define DRV8833_MOTOR
+#define IBT2_MOTOR
+
+const float wheelbase = 0.3747; // [m]
+const float wheel_radius = 0.0635; // [m]
+const float no_load_rps = 9.23; // determined experimentally in [m/s] --> [rad/s]
+
+#ifdef DRV8833_MOTOR
 #include "drv8833_motor.h"
-#include "ls7184_encoder.h"
-#include "pid_controller.h"
-#include "four_wheel_drive.h"
-
-const float wheelbase = 0.14; // [m]
-const float wheel_radius = 0.065 / 2; // [m]
-const float no_load_rps = 0.4 / wheel_radius; // determined experimentally in [m/s] --> [rad/s]
-
-// Initialize encoder parameter struct
-// {byte clk_pin (byte), byte dir_pin, float counts_per_rev, float sample_period, float filt_tc}
-Farmaid::EncoderParams fleft_encoder_p = {19, 23, 1920.0, control_period / 1000.0, 4.0 * control_period / 1000.0};
-Farmaid::EncoderParams fright_encoder_p = {18, 22, 1920.0, control_period / 1000.0, 4.0 * control_period / 1000.0};
-Farmaid::EncoderParams rright_encoder_p = {2, 25, 1920.0, control_period / 1000.0, 4.0 * control_period / 1000.0};
-Farmaid::EncoderParams rleft_encoder_p = {3, 24, 1920.0, control_period / 1000.0, 4.0 * control_period / 1000.0};
-
 // Initialize motor parameter struct
-// {byte pwm_pin, byte dir_pin, unsigned int max_command, float no_load_rps}
+ {byte pwm_pin, byte dir_pin, unsigned int max_command, float no_load_rps}
 Farmaid::MotorParams fleft_motor_p = {5, 4, 255, no_load_rps};
 Farmaid::MotorParams fright_motor_p = {7, 6, 255, no_load_rps};
 Farmaid::MotorParams rright_motor_p = {9, 8, 255, no_load_rps};
 Farmaid::MotorParams rleft_motor_p = {11, 10, 255, no_load_rps};
+#endif
+
+#ifdef IBT2_MOTOR
+#include "ibt2_motor.h"
+// Initialize motor parameter struct
+// {byte pwm_f_pin, byte pwm_r_pin, unsigned int max_command, float no_load_rps}
+Farmaid::MotorParams fleft_motor_p = {5, 4, 255, no_load_rps};
+Farmaid::MotorParams fright_motor_p = {7, 6, 255, no_load_rps};
+Farmaid::MotorParams rright_motor_p = {9, 8, 255, no_load_rps};
+Farmaid::MotorParams rleft_motor_p = {11, 10, 255, no_load_rps};
+#endif
+
+#include "ls7184_encoder.h"
+#include "pid_controller.h"
+#include "four_wheel_drive.h"
+
+// Initialize encoder parameter struct
+// {byte clk_pin (byte), byte dir_pin, float counts_per_rev, float sample_period, float filt_tc}
+Farmaid::EncoderParams fleft_encoder_p = {19, 23, 748.65*2, control_period / 1000.0, 4.0 * control_period / 1000.0};
+Farmaid::EncoderParams fright_encoder_p = {18, 22, 748.65*2, control_period / 1000.0, 4.0 * control_period / 1000.0};
+Farmaid::EncoderParams rright_encoder_p = {2, 25, 748.65*2, control_period / 1000.0, 4.0 * control_period / 1000.0};
+Farmaid::EncoderParams rleft_encoder_p = {3, 24, 748.65*2, control_period / 1000.0, 4.0 * control_period / 1000.0};
 
 // Initialize PID controller parameter struct
 // {float p_gain, float i_gain, float d_gain, float filt_const, float sample_period}
